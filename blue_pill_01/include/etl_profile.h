842a22fc1bceb4de907ccb73e9d93ff00ae08520
Matheus Marques <code@matheusmbar.com>
2023-11-12 08:22:46 -0300

    doc: describe etl macros

--- blue_pill_01/include/etl_profile.h
+++ blue_pill_01/include/etl_profile.h
@@ -1,8 +1,14 @@
 #ifndef ETL_PROFILE_H
 #define ETL_PROFILE_H
 
-#define ETL_LOG_ERRORS
-#define ETL_VERBOSE_ERRORS
-#define ETL_CHECK_PUSH_POP
+/**
+ * @brief Select the features or options in the ETL by defining the appropriate macros
+ * More information at https://www.etlcpp.com/macros.html
+ */
+
+#define ETL_LOG_ERRORS     /* ETL_ASSERT calls the error handler then asserts.*/
+#define ETL_VERBOSE_ERRORS /* If this is defined then error messages and ouput in their long \
+                              form.*/
+#define ETL_CHECK_PUSH_POP /* Pushes and pops to containers are checked for bounds.*/
 
 #endif

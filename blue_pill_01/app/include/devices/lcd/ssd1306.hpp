6dc427167fbaf56471e8e4fc843e75c62a13d6fd
Matheus Marques <code@matheusmbar.com>
2023-12-10 23:00:47 -0300

    fix: replace macros with constextpr

--- blue_pill_01/app/include/devices/lcd/ssd1306.hpp
+++ blue_pill_01/app/include/devices/lcd/ssd1306.hpp
@@ -2,52 +2,53 @@
 
 #include <etl/array.h>
 #include <etl/array_view.h>
+#include <etl/byte.h>
 #include <etl/vector.h>
 #include <libopencm3/stm32/i2c.h>
 
 /// fit into the SSD1306_ naming scheme
-#define SSD1306_BLACK 0    ///< Draw 'off' pixels
-#define SSD1306_WHITE 1    ///< Draw 'on' pixels
-#define SSD1306_INVERSE 2  ///< Invert pixels
+constexpr uint8_t SSD1306_BLACK{0};    ///< Draw 'off' pixels
+constexpr uint8_t SSD1306_WHITE{1};    ///< Draw 'on' pixels
+constexpr uint8_t SSD1306_INVERSE{2};  ///< Invert pixels
 
-#define SSD1306_MEMORYMODE 0x20
-#define SSD1306_COLUMNADDR 0x21
-#define SSD1306_PAGEADDR 0x22
-#define SSD1306_SETCONTRAST 0x81
-#define SSD1306_CHARGEPUMP 0x8D
-#define SSD1306_SEGREMAP 0xA0
-#define SSD1306_DISPLAYALLON_RESUME 0xA4
-#define SSD1306_DISPLAYALLON 0xA5
-#define SSD1306_NORMALDISPLAY 0xA6
-#define SSD1306_INVERTDISPLAY 0xA7
-#define SSD1306_SETMULTIPLEX 0xA8
-#define SSD1306_DISPLAYOFF 0xAE
-#define SSD1306_DISPLAYON 0xAF
-#define SSD1306_COMSCANINC 0xC0
-#define SSD1306_COMSCANDEC 0xC8
-#define SSD1306_SETDISPLAYOFFSET 0xD3
-#define SSD1306_SETDISPLAYCLOCKDIV 0xD5
-#define SSD1306_SETPRECHARGE 0xD9
-#define SSD1306_SETCOMPINS 0xDA
-#define SSD1306_SETVCOMDETECT 0xDB
+constexpr uint8_t SSD1306_MEMORYMODE{0x20};
+constexpr uint8_t SSD1306_COLUMNADDR{0x21};
+constexpr uint8_t SSD1306_PAGEADDR{0x22};
+constexpr uint8_t SSD1306_SETCONTRAST{0x81};
+constexpr uint8_t SSD1306_CHARGEPUMP{0x8D};
+constexpr uint8_t SSD1306_SEGREMAP{0xA0};
+constexpr uint8_t SSD1306_DISPLAYALLON_RESUME{0xA4};
+constexpr uint8_t SSD1306_DISPLAYALLON{0xA5};
+constexpr uint8_t SSD1306_NORMALDISPLAY{0xA6};
+constexpr uint8_t SSD1306_INVERTDISPLAY{0xA7};
+constexpr uint8_t SSD1306_SETMULTIPLEX{0xA8};
+constexpr uint8_t SSD1306_DISPLAYOFF{0xAE};
+constexpr uint8_t SSD1306_DISPLAYON{0xAF};
+constexpr uint8_t SSD1306_COMSCANINC{0xC0};
+constexpr uint8_t SSD1306_COMSCANDEC{0xC8};
+constexpr uint8_t SSD1306_SETDISPLAYOFFSET{0xD3};
+constexpr uint8_t SSD1306_SETDISPLAYCLOCKDIV{0xD5};
+constexpr uint8_t SSD1306_SETPRECHARGE{0xD9};
+constexpr uint8_t SSD1306_SETCOMPINS{0xDA};
+constexpr uint8_t SSD1306_SETVCOMDETECT{0xDB};
 
-#define SSD1306_SETLOWCOLUMN 0x00
-#define SSD1306_SETHIGHCOLUMN 0x10
-#define SSD1306_SETSTARTLINE 0x40
+constexpr uint8_t SSD1306_SETLOWCOLUMN{0x00};
+constexpr uint8_t SSD1306_SETHIGHCOLUMN{0x10};
+constexpr uint8_t SSD1306_SETSTARTLINE{0x40};
 
-#define SSD1306_EXTERNALVCC 0x01   ///< External display voltage source
-#define SSD1306_SWITCHCAPVCC 0x02  ///< Gen. display voltage from 3.3V
+constexpr uint8_t SSD1306_EXTERNALVCC{0x01};   ///< External display voltage source
+constexpr uint8_t SSD1306_SWITCHCAPVCC{0x02};  ///< Gen. display voltage from 3.3V
 
-#define SSD1306_CONTROL_COMMAND 0x00  ///< Next byte is a command
-#define SSD1306_CONTROL_DATA 0x40     ///< Next byte is data for GDDRAM
+constexpr uint8_t SSD1306_CONTROL_COMMAND{0x00};  ///< Next byte is a command
+constexpr uint8_t SSD1306_CONTROL_DATA{0x40};     ///< Next byte is data for GDDRAM
 
-#define SSD1306_RIGHT_HORIZONTAL_SCROLL 0x26               ///< Init rt scroll
-#define SSD1306_LEFT_HORIZONTAL_SCROLL 0x27                ///< Init left scroll
-#define SSD1306_VERTICAL_AND_RIGHT_HORIZONTAL_SCROLL 0x29  ///< Init diag scroll
-#define SSD1306_VERTICAL_AND_LEFT_HORIZONTAL_SCROLL 0x2A   ///< Init diag scroll
-#define SSD1306_DEACTIVATE_SCROLL 0x2E                     ///< Stop scroll
-#define SSD1306_ACTIVATE_SCROLL 0x2F                       ///< Start scroll
-#define SSD1306_SET_VERTICAL_SCROLL_AREA 0xA3              ///< Set scroll range
+constexpr uint8_t SSD1306_RIGHT_HORIZONTAL_SCROLL{0x26};               ///< Init rt scroll
+constexpr uint8_t SSD1306_LEFT_HORIZONTAL_SCROLL{0x27};                ///< Init left scroll
+constexpr uint8_t SSD1306_VERTICAL_AND_RIGHT_HORIZONTAL_SCROLL{0x29};  ///< Init diag scroll
+constexpr uint8_t SSD1306_VERTICAL_AND_LEFT_HORIZONTAL_SCROLL{0x2A};   ///< Init diag scroll
+constexpr uint8_t SSD1306_DEACTIVATE_SCROLL{0x2E};                     ///< Stop scroll
+constexpr uint8_t SSD1306_ACTIVATE_SCROLL{0x2F};                       ///< Start scroll
+constexpr uint8_t SSD1306_SET_VERTICAL_SCROLL_AREA{0xA3};              ///< Set scroll range
 
 template <const uint16_t width, const uint16_t height>
 class SSD1306 {

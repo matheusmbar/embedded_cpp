0f58d04e8364ad2f6f5d5def3a778c75b07af3a0
Matheus Marques <code@matheusmbar.com>
2024-03-23 17:00:14 -0300

    feat: update FreeRTOS version

--- blue_pill_01/cmake/freertos_kernel.cmake
+++ blue_pill_01/cmake/freertos_kernel.cmake
@@ -3,15 +3,21 @@ include(FetchContent)
 
 FetchContent_Declare( freertos_kernel
   GIT_REPOSITORY https://github.com/FreeRTOS/FreeRTOS-Kernel.git
-  GIT_TAG        V10.5.1
+  GIT_TAG        V11.0.1
   GIT_SHALLOW    TRUE
   GIT_PROGRESS   TRUE
   SOURCE_DIR     "${CMAKE_SOURCE_DIR}/thirdy-party/freertos_kernel"
 )
 
+add_library(freertos_config INTERFACE)
+
 # Set path to folder containing FreeRTOSConfig.h
-set( FREERTOS_CONFIG_FILE_DIRECTORY
-    "${CMAKE_SOURCE_DIR}/lib/include" CACHE STRING "" FORCE)
+target_include_directories(
+  freertos_config
+  INTERFACE
+  "${CMAKE_SOURCE_DIR}/lib/include"
+)
+
 # Select memory allocation implementation
 set( FREERTOS_HEAP "4" CACHE STRING "" FORCE)
 # Select the native compile PORT
